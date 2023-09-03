69421927fd82f9bd6b382537dad1fee1d78e6ca5
James Tennant <jtennant@ucsc.edu>
2023-09-03 16:06:28 -0700

    monster input

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../model/model.dart';
 import '../components/monster/list.dart';
+import '../components/monster/input.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,6 +12,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var monsters = context.watch<ViewModel>().monsters;
 
-    return MonsterList(monsters: monsters);
+    return Column(
+      children: [
+        Expanded(child: MonsterList(monsters: monsters)),
+        Container(
+          padding: const EdgeInsets.all(20),
+          child: const MonsterInput(),
+        ),
+      ],
+    );
   }
 }
