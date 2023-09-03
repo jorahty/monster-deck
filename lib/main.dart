1eafbef454a7b139190e5bdbbdfc58358a2e459d
James Tennant <jtennant@ucsc.edu>
2023-09-03 10:41:33 -0700

    config theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,66 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const colorSchemeSeed = Colors.pink;
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        colorSchemeSeed: colorSchemeSeed,
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: colorSchemeSeed,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Hello'),
+            ),
+            const SizedBox(height: 20),
+            FilledButton(
+              onPressed: () {},
+              child: const Text('Hello'),
+            ),
+            const SizedBox(height: 20),
+            FloatingActionButton.extended(
+              onPressed: () {},
+              label: const Text('Hello'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
